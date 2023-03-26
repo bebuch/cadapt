859e86005cd09d70d7f2c047591e8debcafc74d2
Benjamin Buch <benni.buch@gmail.com>
2023-03-26 01:19:33 +0100

    add more c_str unit tests

--- test/c_str.cpp
+++ test/c_str.cpp
@@ -22,6 +22,26 @@ TEST(c_str_test, c_str) {
     EXPECT_FALSE(is_equal("te\0st"sv, unverified_c_str("te\0st")));
     EXPECT_TRUE(is_equal("test"sv, c_str("test")));
     EXPECT_THROW(is_equal("te\0st"sv, c_str("te\0st")), std::logic_error);
+
+    EXPECT_TRUE(is_equal(L"test"sv, unverified_c_str(L"test")));
+    EXPECT_FALSE(is_equal(L"te\0st"sv, unverified_c_str(L"te\0st")));
+    EXPECT_TRUE(is_equal(L"test"sv, c_str(L"test")));
+    EXPECT_THROW(is_equal(L"te\0st"sv, c_str(L"te\0st")), std::logic_error);
+
+    EXPECT_TRUE(is_equal(U"test"sv, unverified_c_str(U"test")));
+    EXPECT_FALSE(is_equal(U"te\0st"sv, unverified_c_str(U"te\0st")));
+    EXPECT_TRUE(is_equal(U"test"sv, c_str(U"test")));
+    EXPECT_THROW(is_equal(U"te\0st"sv, c_str(U"te\0st")), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u"test"sv, unverified_c_str(u"test")));
+    EXPECT_FALSE(is_equal(u"te\0st"sv, unverified_c_str(u"te\0st")));
+    EXPECT_TRUE(is_equal(u"test"sv, c_str(u"test")));
+    EXPECT_THROW(is_equal(u"te\0st"sv, c_str(u"te\0st")), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u8"test"sv, unverified_c_str(u8"test")));
+    EXPECT_FALSE(is_equal(u8"te\0st"sv, unverified_c_str(u8"te\0st")));
+    EXPECT_TRUE(is_equal(u8"test"sv, c_str(u8"test")));
+    EXPECT_THROW(is_equal(u8"te\0st"sv, c_str(u8"te\0st")), std::logic_error);
 }
 
 TEST(c_str_test, std_string) {
@@ -29,6 +49,26 @@ TEST(c_str_test, std_string) {
     EXPECT_FALSE(is_equal("te\0st"sv, unverified_c_str("te\0st"s)));
     EXPECT_TRUE(is_equal("test"sv, c_str("test"s)));
     EXPECT_THROW(is_equal("te\0st"sv, c_str("te\0st"s)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(L"test"sv, unverified_c_str(L"test"s)));
+    EXPECT_FALSE(is_equal(L"te\0st"sv, unverified_c_str(L"te\0st"s)));
+    EXPECT_TRUE(is_equal(L"test"sv, c_str(L"test"s)));
+    EXPECT_THROW(is_equal(L"te\0st"sv, c_str(L"te\0st"s)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(U"test"sv, unverified_c_str(U"test"s)));
+    EXPECT_FALSE(is_equal(U"te\0st"sv, unverified_c_str(U"te\0st"s)));
+    EXPECT_TRUE(is_equal(U"test"sv, c_str(U"test"s)));
+    EXPECT_THROW(is_equal(U"te\0st"sv, c_str(U"te\0st"s)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u"test"sv, unverified_c_str(u"test"s)));
+    EXPECT_FALSE(is_equal(u"te\0st"sv, unverified_c_str(u"te\0st"s)));
+    EXPECT_TRUE(is_equal(u"test"sv, c_str(u"test"s)));
+    EXPECT_THROW(is_equal(u"te\0st"sv, c_str(u"te\0st"s)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u8"test"sv, unverified_c_str(u8"test"s)));
+    EXPECT_FALSE(is_equal(u8"te\0st"sv, unverified_c_str(u8"te\0st"s)));
+    EXPECT_TRUE(is_equal(u8"test"sv, c_str(u8"test"s)));
+    EXPECT_THROW(is_equal(u8"te\0st"sv, c_str(u8"te\0st"s)), std::logic_error);
 }
 
 TEST(c_str_test, std_string_view) {
@@ -36,6 +76,26 @@ TEST(c_str_test, std_string_view) {
     EXPECT_FALSE(is_equal("te\0st"sv, unverified_c_str("te\0st"sv)));
     EXPECT_TRUE(is_equal("test"sv, c_str("test"sv)));
     EXPECT_THROW(is_equal("te\0st"sv, c_str("te\0st"sv)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(L"test"sv, unverified_c_str(L"test"sv)));
+    EXPECT_FALSE(is_equal(L"te\0st"sv, unverified_c_str(L"te\0st"sv)));
+    EXPECT_TRUE(is_equal(L"test"sv, c_str(L"test"sv)));
+    EXPECT_THROW(is_equal(L"te\0st"sv, c_str(L"te\0st"sv)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(U"test"sv, unverified_c_str(U"test"sv)));
+    EXPECT_FALSE(is_equal(U"te\0st"sv, unverified_c_str(U"te\0st"sv)));
+    EXPECT_TRUE(is_equal(U"test"sv, c_str(U"test"sv)));
+    EXPECT_THROW(is_equal(U"te\0st"sv, c_str(U"te\0st"sv)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u"test"sv, unverified_c_str(u"test"sv)));
+    EXPECT_FALSE(is_equal(u"te\0st"sv, unverified_c_str(u"te\0st"sv)));
+    EXPECT_TRUE(is_equal(u"test"sv, c_str(u"test"sv)));
+    EXPECT_THROW(is_equal(u"te\0st"sv, c_str(u"te\0st"sv)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u8"test"sv, unverified_c_str(u8"test"sv)));
+    EXPECT_FALSE(is_equal(u8"te\0st"sv, unverified_c_str(u8"te\0st"sv)));
+    EXPECT_TRUE(is_equal(u8"test"sv, c_str(u8"test"sv)));
+    EXPECT_THROW(is_equal(u8"te\0st"sv, c_str(u8"te\0st"sv)), std::logic_error);
 }
 
 TEST(c_str_test, c_str_view) {
@@ -43,6 +103,26 @@ TEST(c_str_test, c_str_view) {
     EXPECT_FALSE(is_equal("te\0st"sv, unverified_c_str("te\0st"_sv)));
     EXPECT_TRUE(is_equal("test"sv, c_str("test"_sv)));
     EXPECT_THROW(is_equal("te\0st"sv, c_str("te\0st"_sv)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(L"test"sv, unverified_c_str(L"test"_sv)));
+    EXPECT_FALSE(is_equal(L"te\0st"sv, unverified_c_str(L"te\0st"_sv)));
+    EXPECT_TRUE(is_equal(L"test"sv, c_str(L"test"_sv)));
+    EXPECT_THROW(is_equal(L"te\0st"sv, c_str(L"te\0st"_sv)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(U"test"sv, unverified_c_str(U"test"_sv)));
+    EXPECT_FALSE(is_equal(U"te\0st"sv, unverified_c_str(U"te\0st"_sv)));
+    EXPECT_TRUE(is_equal(U"test"sv, c_str(U"test"_sv)));
+    EXPECT_THROW(is_equal(U"te\0st"sv, c_str(U"te\0st"_sv)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u"test"sv, unverified_c_str(u"test"_sv)));
+    EXPECT_FALSE(is_equal(u"te\0st"sv, unverified_c_str(u"te\0st"_sv)));
+    EXPECT_TRUE(is_equal(u"test"sv, c_str(u"test"_sv)));
+    EXPECT_THROW(is_equal(u"te\0st"sv, c_str(u"te\0st"_sv)), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u8"test"sv, unverified_c_str(u8"test"_sv)));
+    EXPECT_FALSE(is_equal(u8"te\0st"sv, unverified_c_str(u8"te\0st"_sv)));
+    EXPECT_TRUE(is_equal(u8"test"sv, c_str(u8"test"_sv)));
+    EXPECT_THROW(is_equal(u8"te\0st"sv, c_str(u8"te\0st"_sv)), std::logic_error);
 }
 
 #if __has_include(<QString>)
@@ -51,5 +131,20 @@ TEST(c_str_test, qstring) {
     EXPECT_FALSE(is_equal("te\0st"sv, unverified_c_str(QString::fromStdString("te\0st"s))));
     EXPECT_TRUE(is_equal("test"sv, c_str(QString::fromStdString("test"s))));
     EXPECT_THROW(is_equal("te\0st"sv, c_str(QString::fromStdString("te\0st"s))), std::logic_error);
+
+    EXPECT_TRUE(is_equal(L"test"sv, unverified_c_str<wchar_t>(QString::fromStdString("test"s))));
+    EXPECT_FALSE(is_equal(L"te\0st"sv, unverified_c_str<wchar_t>(QString::fromStdString("te\0st"s))));
+    EXPECT_TRUE(is_equal(L"test"sv, c_str<wchar_t>(QString::fromStdString("test"s))));
+    EXPECT_THROW(is_equal(L"te\0st"sv, c_str<wchar_t>(QString::fromStdString("te\0st"s))), std::logic_error);
+
+    EXPECT_TRUE(is_equal(U"test"sv, unverified_c_str<char32_t>(QString::fromStdString("test"s))));
+    EXPECT_FALSE(is_equal(U"te\0st"sv, unverified_c_str<char32_t>(QString::fromStdString("te\0st"s))));
+    EXPECT_TRUE(is_equal(U"test"sv, c_str<char32_t>(QString::fromStdString("test"s))));
+    EXPECT_THROW(is_equal(U"te\0st"sv, c_str<char32_t>(QString::fromStdString("te\0st"s))), std::logic_error);
+
+    EXPECT_TRUE(is_equal(u"test"sv, unverified_c_str<char16_t>(QString::fromStdString("test"s))));
+    EXPECT_FALSE(is_equal(u"te\0st"sv, unverified_c_str<char16_t>(QString::fromStdString("te\0st"s))));
+    EXPECT_TRUE(is_equal(u"test"sv, c_str<char16_t>(QString::fromStdString("test"s))));
+    EXPECT_THROW(is_equal(u"te\0st"sv, c_str<char16_t>(QString::fromStdString("te\0st"s))), std::logic_error);
 }
 #endif
