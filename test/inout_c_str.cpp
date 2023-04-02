bef6ef8ec87ddf454f40dfe854bcd3f00c637f92
Benjamin Buch <benni.buch@gmail.com>
2023-04-02 20:35:44 +0200

    add compile time tests for resize_for_out_ptr and enlarge_for_inout_ptr

--- test/inout_c_str.cpp
+++ test/inout_c_str.cpp
@@ -21,6 +21,17 @@ TEST(inout_c_str, resize_for_out_ptr) {
     test(std::u32string{});
     test(std::u16string{});
     test(std::u8string{});
+
+    static constexpr auto ct_resize = []<typename T>(std::basic_string<T>&& target, std::size_t count) {
+        resize_for_out_ptr(target, count);
+        return std::make_pair(std::basic_string_view(target.c_str()), target.size());
+    };
+
+    static_assert(ct_resize("test"s, 32) == std::make_pair(""sv, std::size_t(32)));
+    static_assert(ct_resize(L"test"s, 32) == std::make_pair(L""sv, std::size_t(32)));
+    static_assert(ct_resize(U"test"s, 32) == std::make_pair(U""sv, std::size_t(32)));
+    static_assert(ct_resize(u"test"s, 32) == std::make_pair(u""sv, std::size_t(32)));
+    static_assert(ct_resize(u8"test"s, 32) == std::make_pair(u8""sv, std::size_t(32)));
 }
 
 TEST(inout_c_str, enlarge_for_inout_ptr) {
@@ -41,4 +52,15 @@ TEST(inout_c_str, enlarge_for_inout_ptr) {
     test(U"test"s);
     test(u"test"s);
     test(u8"test"s);
+
+    static constexpr auto ct_enlarge = []<typename T>(std::basic_string<T>&& target, std::size_t count) {
+        enlarge_for_inout_ptr(target, count);
+        return std::make_pair(std::basic_string_view(target.c_str()), target.size());
+    };
+
+    static_assert(ct_enlarge("test"s, 32) == std::make_pair("test"sv, std::size_t(32)));
+    static_assert(ct_enlarge(L"test"s, 32) == std::make_pair(L"test"sv, std::size_t(32)));
+    static_assert(ct_enlarge(U"test"s, 32) == std::make_pair(U"test"sv, std::size_t(32)));
+    static_assert(ct_enlarge(u"test"s, 32) == std::make_pair(u"test"sv, std::size_t(32)));
+    static_assert(ct_enlarge(u8"test"s, 32) == std::make_pair(u8"test"sv, std::size_t(32)));
 }
